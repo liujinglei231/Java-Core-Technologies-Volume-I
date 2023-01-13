208408471d9c943b51427fbc9a3b09870934a8f5
liujinglei <1743968695@qq.com>
2023-01-13 15:23:54 +0800

    无

---
+++ Unit3/3.5.2/MathTest.java
@@ -0,0 +1,14 @@
+public class MathTest {
+    /*
+     * Math常用方法
+     * */
+    public static void main(String[] args) {
+//      求数值平方根
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+//      幂运算
+        double i = Math.pow(x, 2);
+        System.out.println(i);
+    }
+}
---
+++ Unit3/3.6/StringTest.java
@@ -0,0 +1,15 @@
+public class StringTest {
+    public static void main(String[] args) {
+        /*
+         * 字串(截取字符串)
+         * */
+        String greeting = "Hello";
+        String s=greeting.substring(0,3);
+        System.out.println(s);
+
+        /*
+        * 拼接
+        * */
+
+    }
+}
